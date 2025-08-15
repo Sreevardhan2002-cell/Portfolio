33c77d12cbbc30e5611c5a366bd079a631ca0552
Sreevardhan2002-cell <anuragamkarikum123@gmail.com>
2025-08-15 16:11:59 +0530

    Update in navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import NaveBar from './Component/NavBar'
 import About from './Component/About'
 import './App.css'
+import { BrowserRouter as Router } from 'react-router-dom'
 import AboutContent from './Component/AboutContent'
 import Skill from './Component/skill'
 import Project from './Component/project'
@@ -9,7 +10,9 @@ import Footer from './Component/Footer'
 function App() {
   return (
     <div>
+      <Router>
       <NaveBar />
+      </Router>
       <About />
       <AboutContent />
       <Skill />
--- src/Component/NavBar.jsx
+++ src/Component/NavBar.jsx
@@ -1,5 +1,7 @@
 
+import React from "react";
 import "../CSS/NavBar.css"
+import { Link } from "react-router-dom";
 import About from "./About"
 import AboutContent from "./AboutContent"
 import Skill from "./skill"
@@ -7,16 +9,24 @@ import Project from "./project"
 import Footer from "./Footer"
 function NaveBar(){
 
+    const [open,setopen]=React.useState(false);
+
     const handleToggle = () => {
+        setopen(!open);
         // Handle toggle functionality here
-        const dropdown = document.querySelector('.nav-dropdown');
-        const navLink = document.querySelectorAll('.navbar-link')
-        dropdown.style.display = dropdown.style.display === 'none' ? "flex" : 'none';
-        navLink.forEach(link =>{
-            link.style.display = dropdown.style.display === 'flex' ? 'block' : 'none'
-            link.style.flexBasis = dropdown.style.display === 'flex' ? '10.33%' : ''
-            link.style.cursor = dropdown.style.display === 'flex' ? 'pointer' : 'default'
-        })
+    
+        // const slide= style.width('500px')
+        // if(window.matchMedia("(max-width: 500px)").matches){
+        // const dropdown = document.querySelector('.nav-dropdown');
+        // const navLink = document.querySelectorAll('.nav_drop-link');
+        
+        //     dropdown.style.display = dropdown.style.display === 'none' ? "flex" : 'none';
+
+        //     navLink.forEach(link =>{
+        //         link.style.display = dropdown.style.display === 'flex' ? 'block' : 'none'
+        //         link.style.flexBasis = dropdown.style.display === 'flex' ? '10.33%' : ''
+        //         link.style.cursor = dropdown.style.display === 'flex' ? 'pointer' : 'default'
+        //     })
 
     }
 
@@ -30,14 +40,24 @@ function NaveBar(){
                 <div className="nav-middle_line common"></div>
                 <div className="nav-bottom_line common"></div>
                 </div>
-            <li className="navbar-links nav-dropdown">
+            <ul className={`navbar-links nav-dropdown${open ? " open" : ""}`}>
 
-                <a href={About} className="navbar-link nav-btn">Home</a>
-                <a href={AboutContent} className="navbar-link nav-btn">About</a>
-                <a href={Skill} className="navbar-link nav-btn">Skill</a>
-                <a href={Project} className="navbar-link nav-btn">Project</a>
-                <a href={Footer} className="navbar-link nav-btn">contact</a>
-            </li>
+                <li>
+                    <Link to="/" className="navbar-link nav_drop-link nav-btn">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about" className="navbar-link nav_drop-link nav-btn">About</Link>
+                </li>
+                <li>
+                    <Link to="/skill" className="navbar-link nav_drop-link nav-btn">Skill</Link>
+                </li>
+                <li>
+                    <Link to="/project" className="navbar-link nav_drop-link nav-btn">Project</Link>
+                </li>
+                <li>
+                    <Link to="/contact" className="navbar-link nav_drop-link nav-btn">contact</Link>
+                </li>
+            </ul>
         </nav>
     )
 }
